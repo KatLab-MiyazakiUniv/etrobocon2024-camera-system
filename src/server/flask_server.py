d7679337c9db128b890307b187ee4b99579e229c
KeiyaNimura <asxx1375@gmail.com>
2024-07-31 15:50:20 +0900

    update: サーバのIPアドレスを取得できるように変更

--- src/server/flask_server.py
+++ src/server/flask_server.py
@@ -5,6 +5,7 @@
 """
 
 import os
+import socket
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
@@ -35,11 +36,15 @@ def getImageFile() -> jsonify:
         return jsonify({"message": "File uploaded successfully",
                         "filePath": filePath}), 200
 
-
+print(os.uname()[1])
 # ポート番号の設定
 if __name__ == "__main__":
     ip = "127.0.0.1"
     host = os.uname()[1]
     if host == "KatLabLaptop":
-        ip = "    "
+        #参照 https://qiita.com/suzu12/items/b5c3d16aae55effb67c0
+        connect_interface = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        connect_interface.connect(("8.8.8.8", 80))
+        ip = connect_interface.getsockname()[0]
+        connect_interface.close()
     app.run(host=ip, port=8000)
