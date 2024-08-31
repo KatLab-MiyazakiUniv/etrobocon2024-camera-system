aeb2d68d0f9e277f35e876179b00a8199d1bf21a
KeiyaNimura <asxx1375@gmail.com>
2024-08-31 12:58:01 +0900

    update: 試走会での変更を統合

--- src/server/flask_server.py
+++ src/server/flask_server.py
@@ -4,11 +4,14 @@
 @author Keiya121 CHIHAYATAKU KakinokiKanta
 """
 
+from fileinput import filename
 import os
 import sys
 import socket
 import platform
 from flask_cors import CORS
+from csv_to_json import CSVToJSONConverter
+from official_interface import OfficialInterface
 
 from flask import Flask, request, jsonify, send_file
 
@@ -50,6 +53,11 @@ def get_image() -> jsonify:
     # src/server/image_dataに、受信したファイルを保存する。
     file_path = os.path.join(upload_folder, file_name)
     file.save(file_path)
+
+    # TODO: 現在は、1枚目のフィグ画像、プラレール画像の場合に競技システムへアップロードしている
+    if file_name == 'Fig_1.jpeg' or file_name == 'Pla.jpeg':
+        OfficialInterface.upload_snap(file_path)
+    
     return jsonify({"message": "File uploaded successfully"}), 200
 
 # '/run-log'へのPOSTリクエストに対する操作
