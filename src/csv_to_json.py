87a434391cad34158be7c30435da3ec1075e6248
KeiyaNimura <asxx1375@gmail.com>
2024-09-08 17:02:33 +0900

    refactor: docstringの記述

--- src/csv_to_json.py
+++ src/csv_to_json.py
@@ -14,7 +14,11 @@ class CSVToJSONConverter:
     """CSVファイルをJSONファイルに変換するクラス."""
 
     def __init__(self, csv_file_path: str) -> None:
-        """コンストラクタ."""
+        """コンストラクタ.
+
+        Args:
+            csv_file_path (str): CSVファイルのパス
+        """
         self.csv_file_path = csv_file_path
         self.json_file_path = self._get_json_file_path()
 
@@ -24,18 +28,26 @@ class CSVToJSONConverter:
         self._write_json(data)
 
     def _read_csv(self) -> List[dict]:
-        """CSVファイルを読み込み、辞書のリストを返す."""
-        data = []
+        """CSVファイルを読み込み、辞書のリストを返す.
+
+        Return:
+              run_log_data (List[dict]): 辞書形式の走行ログデータ
+        """
+        run_log_data = []
         with open(self.csv_file_path, mode='r', encoding='utf-8') as csv_file:
             reader = csv.DictReader(csv_file, fieldnames=[
                 'brightness', 'rightPWM', 'leftPWM', 'R', 'G', 'B'])
             for row in reader:
-                data.append(row)
-        return data
+                run_log_data.append(row)
+        return run_log_data
 
-    def _write_json(self, data: List[dict]) -> None:
-        """データをJSONファイルに書き込む."""
-        json_data = {'runLog': data}
+    def _write_json(self, run_log_data: List[dict]) -> None:
+        """データをJSONファイルに書き込む.
+
+        Args:
+            run_log_data (List[dict]): 辞書形式の走行ログデータ
+        """
+        json_data = {'runLog': run_log_data}
 
         # JSONファイルの保存先フォルダーを確認し、存在しない場合は作成
         os.makedirs(os.path.dirname(self.json_file_path), exist_ok=True)
@@ -45,6 +57,12 @@ class CSVToJSONConverter:
             json.dump(json_data, json_file, ensure_ascii=False, indent=4)
 
     def _get_json_file_path(self) -> str:
-        """JSONファイルのパスを作成する."""
+        """JSONファイルのパスを作成する.
+
+        Return:
+              json_filepath (str): jsonファイルのパス
+        """
         base, _ = os.path.splitext(os.path.basename(self.csv_file_path))
-        return os.path.join('src', 'server', 'run_log_json', base + '.json')
+        json_filepath = os.path.join(
+            'src', 'server', 'run_log_json', base + '.json')
+        return json_filepath
