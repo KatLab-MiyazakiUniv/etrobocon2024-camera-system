c845b6c1fc7cc78e0f459dc31140efcc6af7513d
KakinokiKanta <kanta12621@gmail.com>
2024-11-15 20:37:00 +0900

    update: 競技システム用ラズパイのIP割り振りの変更に対応

--- scripts/run_official_server.sh
+++ scripts/run_official_server.sh
@@ -6,4 +6,4 @@
 #   - テスト用の競技システムとLANケーブルで接続している
 
 RUN_SERVER_COMMAND="cd /opt/compesys && sudo npm run start"
-ssh taki@192.168.11.26 "${RUN_SERVER_COMMAND}"
+ssh taki@192.168.11.18 "${RUN_SERVER_COMMAND}"
